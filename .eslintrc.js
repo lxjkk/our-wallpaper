44d51a71e8fc2c68873e64a3c8c8c891a9589b5c
lxjkk <lxj1999902@163.com>
2022-01-15 11:50:00 +0800

    eslint 适配项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
+// jsx适配
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  "parser": "babel-eslint",
+  "extends": [
+      "airbnb",
+      "prettier",
+      "prettier/react"
+  ],
+  "env": {
+      "browser": true
+  },
+  "plugins": [
+      "react",
+      "jsx-a11y",
+      "import",
+      "prettier"
+  ],
+  "rules": {
+      "no-console": "off",
+      "linebreak-style": "off",
+      "react/jsx-filename-extension": [
+          1,
+          {
+              "extensions": [".js", ".jsx"]
+          }
+      ],
+      "react/react-in-jsx-scope": 0,
+      "prettier/prettier": [
+          "error",
+          {
+              "trailingComma": "es5",
+              "singleQuote": true,
+              "printWidth": 120
+          }
+      ]
+  }
+}
